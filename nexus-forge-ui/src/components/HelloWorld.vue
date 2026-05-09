1d9cbf12f7e790d36c8aed235d8ffa2269012d2d
VasyaMambo <pexijl@foxmail.com>
2026-05-09 13:57:15 +0800

    init 前端

---
+++ nexus-forge-ui/src/components/HelloWorld.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold underline text-blue-500">
+    Hello Tailwind v4!
+  </h1>
+  <Button label="Submit" />
+</template>
