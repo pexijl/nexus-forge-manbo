20f785ba04f10a34b9fd5324218f737b4583f7c8
VasyaMambo <pexijl@foxmail.com>
2026-05-09 12:49:43 +0800

    init

---
+++ nexus-forge-web/src/main/java/com/nexusforge/NexusForgeApplication.java
@@ -0,0 +1,13 @@
+package com.nexusforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusForgeApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(NexusForgeApplication.class, args);
+	}
+
+}
